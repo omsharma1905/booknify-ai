2e103e964ffdeff2e89eb2c0cbb056a51400f040
Om Sharma <oms177794@gmail.com>
2025-12-30 17:16:57 +0530

    Fix backend for Render

--- backend/server.js
+++ backend/server.js
@@ -43,3 +43,11 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    uptime: process.uptime(),
+    message: "Booknify backend running"
+  });
+});
