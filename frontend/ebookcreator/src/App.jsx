7f99e7a7313967d671224c37777939b22251a237
Om Sharma <oms177794@gmail.com>
2026-05-25 06:39:01 +0530

    Clean up in App.jsx
    Removed comments for public and protected routes.

--- frontend/ebookcreator/src/App.jsx
+++ frontend/ebookcreator/src/App.jsx
@@ -13,12 +13,10 @@ const App = () => {
   return (
     <div>
       <Routes>
-        {/* Public Routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
 
-        {/* Protected Routes */}
         <Route path='/dashboard' element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
         <Route path='/editor/:bookId' element={<ProtectedRoute><EditorPage /></ProtectedRoute>} />
         <Route path='/view-book/:bookId' element={<ProtectedRoute><ViewBookPage /></ProtectedRoute>} />
